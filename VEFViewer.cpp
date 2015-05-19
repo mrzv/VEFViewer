9e49e01e40c68e3566777e397808411b24242f20
Dmitriy Morozov <dmitriy@mrzv.org>
2015-05-18 18:32:39 -0700

    Removed some unnecessary includes

--- VEFViewer.cpp
+++ VEFViewer.cpp
@@ -4,19 +4,10 @@
 #include <nanogui/window.h>
 #include <nanogui/layout.h>
 #include <nanogui/label.h>
-#include <nanogui/checkbox.h>
 #include <nanogui/button.h>
-#include <nanogui/toolbutton.h>
-#include <nanogui/popupbutton.h>
-#include <nanogui/combobox.h>
-#include <nanogui/progressbar.h>
 #include <nanogui/entypo.h>
-#include <nanogui/messagedialog.h>
 #include <nanogui/textbox.h>
 #include <nanogui/slider.h>
-#include <nanogui/imagepanel.h>
-#include <nanogui/imageview.h>
-#include <nanogui/vscrollpanel.h>
 
 #if defined(WIN32)
 #include <windows.h>
